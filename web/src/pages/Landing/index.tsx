56d5e00e9792aa3bba56294f59d53096ccb1b54c
Denis M. Ladeira <denisladeira1@gmail.com>
2020-08-04 20:31:35 -0300

    finish landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -4,6 +4,10 @@ import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 
 import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
 
 function Landing() {
   return (
@@ -20,10 +24,22 @@ function Landing() {
           className="hero-image"
         />
 
-        <div className="button-container">
-          <a href="" className="study" />
-          <a href="" className="study" />
+        <div className="buttons-container">
+          <a href="" className="study">
+            <img src={studyIcon} alt="Estudar" />
+            Estudar
+          </a>
+
+          <a href="" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas" />
+            Dar aulas
+          </a>
         </div>
+
+        <span className="total-connections">
+          Total de 200 conexões já realizadas
+          <img src={purpleHeartIcon} alt="Coração roxo" />
+        </span>
       </div>
     </div>
   );
