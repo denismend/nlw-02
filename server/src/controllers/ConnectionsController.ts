9bca3389f32945a9baf151de70c7baa32e35ae86
Denis M. Ladeira <denisladeira1@gmail.com>
2020-08-06 00:28:36 -0300

    add cors

--- server/src/controllers/ConnectionsController.ts
+++ server/src/controllers/ConnectionsController.ts
@@ -4,7 +4,11 @@ import db from "../database/connection";
 
 export default class ConnectionsController {
   async index(request: Request, response: Response) {
-    const connections = 
+    const totalConnections = await db('connections').count('* as total');
+
+    const { total } = totalConnections[0];
+
+    return response.json({ total });
   }
 
   async create(request: Request, response: Response) {
