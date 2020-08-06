4acaf665479116e4ce2c6f04bc81f62c3cfbabb7
Denis M. Ladeira <denisladeira1@gmail.com>
2020-08-05 22:11:53 -0300

    insert in tables

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,43 @@
 import express from 'express';
+import db from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+routes.post('/classes', async (request, response) => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    subject,
+    cost,
+    schedule
+  } = request.body;
+
+  const insertedUsersIds = await db('users').insert({
+    name,
+    avatar,
+    whatsapp,
+    bio
+  });
+  
+  const user_id = insertedUsersIds[0];
+
+  const insertedClassesIds = await db('classes').insert({
+    subject,
+    cost,
+    user_id
+  });
+
+  const class_id = insertedClassesIds[0];
+  
+  const classSchedule = schedule.map(scheduleItem => {
+    return {
+      
+    }
+  })
+
+  return response.send();
 });
 
 export default routes;
