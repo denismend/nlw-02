d7a1bae62418452029451225fe3bad24843f6f10
Denis M. Ladeira <denisladeira1@gmail.com>
2020-08-06 21:10:44 -0300

    teacher form almost finished

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 import './styles.css';
 
@@ -11,20 +12,12 @@ function TeacherList() {
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
+          <Input name="Subject" label="Matéria" />
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da Semana</label>
-            <input type="text" id="week_day" />
-          </div>
+          <Input name="week_day" label="Dia da Semana" />
 
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Input type="time" name="time" label="Hora" />
+
         </form>
       </PageHeader>
 
