fdc551d76037bb43e0a66b4e7ad67b1d1ffa0a5f
denismend <denisladeira1@gmail.com>
2020-08-08 17:18:37 -0300

    configure navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,7 +8,7 @@ import { Archivo_400Regular, Archivo_700Bold, useFonts }
 import { Poppins_400Regular, Poppins_600SemiBold } 
   from '@expo-google-fonts/poppins';
 
-import Landing from './src/pages/Landing';
+import AppStack from './src/routes/AppStack';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -24,7 +24,7 @@ export default function App() {
   else {
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style="light" />
       </>
     );
