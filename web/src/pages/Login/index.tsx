31d1ba631c253d4967972574a9ad061240a1853f
denismend <denisladeira1@gmail.com>
2020-08-22 15:45:48 -0300

    login page

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 
-
-import logoImg from '../../assets/images/logo.svg';
+import logoImg from '../../assets/images/background.svg';
 
 import './styles.css';
 
 const Login: React.FC = () => {
   return (
     <div id="page-login">
-      <div id="page-login-content">
-          <div className="login-logo-container">
-              <div className="login-logo">
-                  <img src={logoImg} alt="Proffy"/>
-                  <h2>Your online study platform.</h2>
-              </div>
-          </div>
-        </div>
+      <div className="login-background" />
+
+      <div className="form-container">
+
       </div>
+    </div>
   );
 };
 
