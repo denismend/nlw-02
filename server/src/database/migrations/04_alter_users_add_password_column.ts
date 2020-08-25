c61742f9f1a9fccc8368c10c87445d439f69f6f8
Denis M. Ladeira <denisladeira1@gmail.com>
2020-08-24 22:23:50 -0300

    add migration to password column in users tabl

--- server/src/database/migrations/04_alter_users_add_password_column.ts
+++ server/src/database/migrations/04_alter_users_add_password_column.ts
@@ -2,10 +2,12 @@ import Knex from 'knex';
 
 export async function up(knex: Knex) {
   return knex.schema.alterTable('users', table => {
-    table.string('password').notNullable();
+    table.string('password').notNullable().defaultTo('');
   });
 }
 
 export async function down(knex: Knex) {
-  //knex.schema.dropTable('connections');
+  knex.schema.table('users', table => {
+    table.dropColumn('password');
+  });
 }
